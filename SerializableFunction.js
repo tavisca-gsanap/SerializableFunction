80ef7bb59f105aab06f458093377184c67915263
Govind Uddhav Sanap <gsanap@affinionds.com>
2019-08-22 22:46:34 +0530

    With returning statusList
    statusList contain status of every function in functionArray.
    status has 3 fields
    index--which defines function index
    status -- actual message returned by function
    isresolved -- bool conatins whether function is resolved(true) or rejected(false)

--- SerializableFunction.js
+++ SerializableFunction.js
@@ -1,32 +1,46 @@
 function function1(resolve,reject){
-setTimeout(() => {
-alert("fis");
-resolve("done");
-//reject("something wrong");
-} , 3000);
-}
-function function2(resolve,reject){
-setTimeout(() => {
-alert("sec");resolve("done")}, 3000);
-}
-function function3(resolve,reject){
-setTimeout(() => {
-alert("thir");resolve("done")}, 3000);
-}
-
-let functionArray=[function1,function2,function3];
-
-function SerializableFunction(functionArray){
-let index=0;
-let funcLen=functionArray.length;
-(function functionCallBack(n,i){
-	if(i>=n){
-		return}
-	else{
-		new Promise(functionArray[i]).then(
-			result => functionCallBack(n,++i)
-		);
+	setTimeout(() => {
+	console.log("first");
+	resolve("done");
+	//reject("something wrong");
+	} , 3000);
 	}
-})(funcLen,index);
-}
-SerializableFunction(functionArray);
+	function function2(resolve,reject){
+	setTimeout(() => {
+	console.log("second");resolve("done")}, 3000);
+	}
+	function function3(resolve,reject){
+	setTimeout(() => {
+	console.log("third");resolve("done")}, 3000);
+	}
+	
+	let functionArray=[function1,function2,function3];
+	
+	function SerializableFunction(functionArray){
+	let index=0;
+	let funcLen=functionArray.length;
+	let statusList=[];
+	function Status(index,status,isresolved) {
+		this.index = index;
+		this.status = status;
+		this.isresolved = isresolved;
+	  }
+	(function functionCallBack(n,i){
+		if(i>=n){
+			return}
+		else{
+			new Promise(functionArray[i]).then(
+				result => {
+					console.log(result);
+					statusList.push(new Status(i,result,true));
+					functionCallBack(n,++i);},
+				reject=>{
+					console.log(reject);
+					statusList.push(new Status(i,reject,false));
+					functionCallBack(n,++i);}
+			);
+		}
+	})(funcLen,index);
+	return statusList;
+	}
+	let statusList = SerializableFunction(functionArray);
